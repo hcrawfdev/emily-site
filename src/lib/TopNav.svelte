760cbb6dd605adb80e41e2f212179737f27a3e7f
hcrawf <electronics.crawford@gmail.com>
2023-03-17 20:50:34 -0400

    init

---
+++ src/lib/TopNav.svelte
@@ -0,0 +1,55 @@
+<div class="top-nav">
+	<a class="name-title" href="/">Emily Crawford</a>
+	<div class="nav-links">
+		<a class="nav-link" href="/about">About</a>
+		<a class="nav-link" href="/service">Service</a>
+		<a class="nav-link" href="/">Shop</a>
+	</div>
+</div>
+
+<style>
+	@font-face {
+		font-family: 'Amsterdam One';
+		font-style: normal;
+		font-weight: 700;
+		font-display: swap;
+		src: url('/fonts/AmsterdamOne.ttf') format('truetype');
+	}
+
+	.name-title {
+		font-family: 'Amsterdam One';
+		padding: 10px;
+		font-size: 1.5em;
+		text-decoration: none;
+		color: black;
+		text-align: center;
+	}
+	.top-nav {
+		grid-area: nav;
+		padding: 5px;
+		margin-bottom: 10px;
+		display: flex;
+		justify-content: space-between;
+		align-content: center;
+	}
+
+	.nav-links {
+		align-self: center;
+		display: flex;
+	}
+
+	.nav-link {
+		padding: 0 20px 0 20px;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.top-nav {
+			flex-direction: column;
+		}
+
+		.nav-link {
+			padding-top: 25px;
+			padding-bottom: 15px;
+		}
+	}
+</style>
---
+++ src/routes/service/+page.svelte
@@ -0,0 +1,47 @@
+<main id="service">
+	<p>
+		Service has been a part of Emily's life since she began volunteering for her parish's annual
+		summer festival after fourth grade. She has received the Presidential Volunteer Service Award
+		twice at the Silver Level. In 2013, Emily Webb was chosen to represent Mount Notre Dame High
+		School at the Southwest Ohio Hugh O' Brian Youth Leadership (HOBY) Seminar. She has since
+		returned every year on the volunteer staff.
+	</p>
+	<ul>
+		<li>
+			2014: Emily returned to the SWOH HOBY Seminar as the Junior Facilitator of group "Sneetches."
+		</li>
+		<li>
+			2015: Emily returned to the SWOH HOBY Seminar as the Junior Facilitator of the "American Heart
+			Association (AHA)" group. She guided her ambassador in helping AHA (the national nonprofit)
+			create an advertising plan for a walk hosted in Dayton that Fall.
+		</li>
+		<li>
+			2016: Emily returned to the SWOH HOBY Seminar as the Assistant to Director of Operations.
+			Throughout the 5 days at the seminar, Emily ran, monitored, and tracked all of the seminar
+			site's social media accounts. She also controlled conference room lighting, microphones, and
+			other operational needs.
+		</li>
+		<li>
+			2017: Emily, again, returned to the SWOH HOBY Seminar as the Assistant to Director of
+			Operations. Amongst the tasks of the 2016 seminar, she undertook the beginning of transferring
+			the seminar's website.
+		</li>
+	</ul>
+	<p>
+		Besides returning each year as a volunteer, she has served on the seminar planning committee and
+		the Alumni Board.
+	</p>
+</main>
+
+<style>
+	main {
+		grid-column-start: 2;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin: 15px;
+        grid-area: content;
+        font-size: 1.5rem;
+	}
+
+</style>
